0b946ba729251efa2f4ef3139a786885aaf20d88
Iza <iza.wojcik.iw@gmail.com>
2019-02-20 14:47:00 +0100

    comit 3

--- src/mainClass.java
+++ src/mainClass.java
@@ -2,7 +2,7 @@
 public class mainClass {
 
 	public static void main(String[] args) {
-System.out.println("first text");
+System.out.println("first text this is somenthin g bee");
 System.out.println("first text");
 System.out.println("master");
 	}
